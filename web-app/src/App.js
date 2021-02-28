94e54c9f87cba67673a02505e5e241285ed96135
Tom Han <tomhyhan@gmail.com>
2021-02-28 13:14:34 -0800

    basic routes with login/borad

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import styles from './app.module.css';
+import Login from './components/login/login';
+import Board from './components/board/board';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route path={['/', '/login']} exact>
+          <Login />
+        </Route>
+        <Route path='/board'>
+          <Board />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ web-app/src/components/board/board.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './board.module.css';
+
+const Board = () => {
+  return <div>this is board</div>;
+};
+
+export default Board;
